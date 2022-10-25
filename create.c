54f4c3b600c749d22237a2f8e7e140874edb8b74
VladSach <vladsach@gmail.com>
2022-10-25 22:55:20 +0300

    Done

--- create.c
+++ create.c
@@ -1,4 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void file_create(char* path);
+void directory_create(char* path);
+
 int main()
 {
     return 0;
+}
+
+
+void file_create(char* path)
+{
+    FILE* file_ptr = fopen(path, "w");
+    fclose(file_ptr);
+}
+
+void directory_create(char* path)
+{
+    mkdir(path, 0644);
 }
